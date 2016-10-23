75b19161c05c1bcc99f9e942d16341a8d91828dd
pika298 <kit2008@live.com>
2016-10-23 17:43:02 +0800

    Incomplet Tasks Count

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -7,14 +7,32 @@ class TodosListCtrl {
 	constructor($scope) {
 		$scope.viewModel(this);
 
+		this.hideCompleted = false;
+
 		this.helpers({
 			tasks() {
+				const selector = {};
+
+				// If hide compmleted is checked, filter tasks
+				if (this.getReactively('hideCompleted')) {
+					selector.checked = {
+						$ne: true
+					};
+				}
+
 				// Show newest tasks at the top
-				return Tasks.find({}, {
+				return Tasks.find(selector, {
 					sort: {
 						createdAt: -1
 					}
 				});
+			},
+			incompleteCount() {
+				return Tasks.find({
+					checked: {
+						$ne: true
+					}
+				}).count();	
 			}
 		})
 	}
